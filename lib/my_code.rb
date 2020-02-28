8083c0a4d274b92d890011fbe70d331694b5e6c5
Ruth Obe <ruthobey@gmail.com>
2020-02-28 16:50:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -37,12 +37,8 @@ def reduce_to_total(source_array, starting_point = 0)
     value += element
   end
   
-  # binding.pry
-  if starting_point != nil 
-    value += starting_point
-  end
-  
-  return value
+  binding.pry
+  value
 end
 
 def reduce_to_all_true(source_array)
