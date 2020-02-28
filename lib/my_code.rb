d4c182d16303ea53270e42463382ff4e2701fce8
Ruth Obe <ruthobey@gmail.com>
2020-02-28 16:26:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,27 +1,27 @@
 def map_to_negativize(source_array)
-  
+  new_arr 
 end
 
 def map_to_no_change(source_array)
-  
+  new_arr
 end
 
 def map_to_double(source_array)
-  
+  new_arr
 end
 
 def map_to_square(source_array)
-  
+  new_arr
 end
 
-def map_to_negativize(source_array)
-  
+def reduce_to_total(source_array)
+  value
 end
 
-def map_to_negativize(source_array)
-  
+def reduce_to_all_true(source_array)
+  value
 end
 
-def map_to_negativize(source_array)
-  
+def reduce_to_any_true(source_array)
+  value
 end
