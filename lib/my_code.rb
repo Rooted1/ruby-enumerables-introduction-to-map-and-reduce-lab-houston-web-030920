3ccf9c57ec7355f8a44f10e46bd0d79135cae367
Ruth Obe <ruthobey@gmail.com>
2020-02-28 16:58:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -50,9 +50,7 @@ end
 def reduce_to_all_true(source_array)
    value = true
   source_array.each do |element|
-    if element
-      value 
-    else 
+    if !element
       value = false 
     end
   end
